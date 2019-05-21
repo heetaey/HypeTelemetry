12d341875cf565ac2114e8eb8ba2b470769fa2aa
Jaewon Jeong <jeongjaewon@seattleu.edu>
2019-05-20 21:24:55 -0700

    Adds new models

---
+++ Project-Telemetry/HypeTeam-ProjectTele/Models/LogViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HypeTeam_ProjectTele.Models
+{
+    public class LogViewModel
+    {
+        public List<LogModel> LogList { get; set; } = new List<LogModel>();
+    }
+}
---
+++ Project-Telemetry/HypeTeam-ProjectTele/Models/ReportViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HypeTeam_ProjectTele.Models
+{
+    public class ReportViewModel
+    {
+        public LogViewModel LogViewModel { get; set; }
+        public int NumberOfUsers { get; set; }
+    }
+}
