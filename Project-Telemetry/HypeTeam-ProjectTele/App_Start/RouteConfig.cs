03715979ffc584957501741746eb97fe9ee5c46b
Heetae Yang <htyang90@gmail.com>
2019-06-03 15:17:14 -0700

    Changed the starting page to login, Added active-state indicator for the navbars

--- Project-Telemetry/HypeTeam-ProjectTele/App_Start/RouteConfig.cs
+++ Project-Telemetry/HypeTeam-ProjectTele/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HypeTeam_ProjectTele
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "UserLogin", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
